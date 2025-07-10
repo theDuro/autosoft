d399fdd4d05bb9d2bb34fc8450bdb21bd4c90de2
theDuro <dawid.durolek@onet.pl>
2025-07-10 08:08:13 +0200

    change login code

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 const Login = ({ onLogin }) => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -9,7 +8,7 @@ const Login = ({ onLogin }) => {
     e.preventDefault();
     setError(null);
     try {
-      const res = await fetch("http://127.0.0.1:5000/api/login", {
+      const res = await fetch("https://autosoftv2-h4eeh8emg3dzceds.germanywestcentral-01.azurewebsites.net/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
